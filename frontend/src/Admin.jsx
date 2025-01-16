39804046db3c2b343918e27c6be1b796f34b4e56
Dileep Nandanam <dileepsnandanam@gmail.com>
2025-01-17 02:55:23 +0530

    fix static paths

---
+++ frontend/src/Admin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TagManage from "./TagManage";
+
+const Admin = () => {
+  return(<TagManage />)
+}
+
+export default Admin;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Admin from "./Admin";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {import.meta.env.VITE_API_URL}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
+}
+
+const Home = () => {
+  return("coming_soon")
 }
 
 export default App
